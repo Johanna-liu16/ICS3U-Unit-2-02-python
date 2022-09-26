cc3bbe73819b9fc5f8de5512e0190bd2cf5c5826
Johanna Liu <johanna.liu@mths.ca>
2022-09-26 15:24:20 +0000

    added

---
+++ rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Created by: Johanna Liu
+# Created on: Sep 2022
+# This program calculates the area and perimeter of a rectangle
+#    with radius inputted from the user
+
+import math
+from operator import le
+
+
+def main():
+    # this function calculates the area and perimeter of a rectangle
+
+    # input
+    length_of_rectangle = int(input("Enter length of a rectangle (mm): "))
+    width_of_rectangle = int(input("Enter width of a rectangle (mm): "))
+
+    # process
+    area_of_rectangle = length_of_rectangle * width_of_rectangle
+    perimeter_of_rectangle = 2 * (length_of_rectangle + width_of_rectangle)
+
+    # output
+    print("")
+    print("Area is {0} mm².".format(area_of_rectangle))
+    print("Perimeter is {0} mm.".format(perimeter_of_rectangle))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
